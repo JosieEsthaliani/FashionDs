d7e605cd0de86aa237fad306fc9a1455d3a05582
JosieEsthaliani <57379583+JosieEsthaliani@users.noreply.github.com>
2021-10-26 09:37:02 +0700

    initial import

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container bg-dark text-white">
+  <div class="row">
+    <div class="col-md p-5 bg-primary">
+      Column
+    </div>
+    <div class="col-md p-5 bg-danger">
+      Column
+    </div>
+    <div class="col-md p-5 bg-secondary">
+      Column
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md p-5 bg-primary">
+      Column
+    </div>
+    <div class="col-md p-5 bg-danger">
+      Column
+    </div>
+    <div class="col-md p-5 bg-secondary">
+      Column
+    </div>
+  </div>
+</div>
+@endsection
